e4c8d700df2b370c01da2431c902ede874038a0f
Victor <vitinho.vgzin@gmail.com>
2025-05-26 16:54:47 -0300

    all

--- api/src/main/java/com/frt/api/models/entity/Disciplina.java
+++ api/src/main/java/com/frt/api/models/entity/Disciplina.java
@@ -1,11 +1,14 @@
 package com.frt.api.models.entity;
 
+import java.util.List;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.Setter;
@@ -26,6 +29,9 @@ public class Disciplina {
 
     @Column(nullable = false)
     private int cargaHoraria;
+
+    @ManyToMany(mappedBy = "disciplinas")
+    private List<Professor> professores;
 
     @ManyToOne
     @JoinColumn(name = "curso_id")
--- api/src/main/java/com/frt/api/models/entity/Professor.java
+++ api/src/main/java/com/frt/api/models/entity/Professor.java
@@ -5,6 +5,8 @@ import java.util.List;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import lombok.Getter;
@@ -25,6 +27,13 @@ public class Professor extends Usuario {
     @ManyToOne
     @JoinColumn(name = "cadastrado_por_id")
     private Administrador cadastradoPor;
+
+    @ManyToMany
+    @JoinTable(
+    name = "professor_disciplina",
+    joinColumns = @JoinColumn(name = "professor_id"),
+    inverseJoinColumns = @JoinColumn(name = "disciplina_id"))
+    private List<Disciplina> disciplinas;
 
     @OneToMany(mappedBy = "professor")
     private List<Reserva> reservas;
