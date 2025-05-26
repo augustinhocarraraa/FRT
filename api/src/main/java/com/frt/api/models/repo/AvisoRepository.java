777cbca4a7be02e0fd825a9f72a00cbabc43972e
Victor <vitinho.vgzin@gmail.com>
2025-05-26 15:56:27 -0300

    all

--- api/src/main/java/com/frt/api/models/repo/AvisoRepository.java
+++ api/src/main/java/com/frt/api/models/repo/AvisoRepository.java
@@ -1,14 +1,14 @@
 package com.frt.api.models.repo;
 
-import com.frt.api.models.entity.Aviso;
-import com.frt.api.models.enums.PublicoAlvo;
-
-import org.springframework.data.jdbc.repository.query.Query;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.query.Param;
-
 import java.time.LocalDateTime;
 import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query; // Adicione esta linha
+import org.springframework.data.repository.query.Param;
+
+import com.frt.api.models.entity.Aviso;
+import com.frt.api.models.enums.PublicoAlvo;
 
 public interface AvisoRepository extends JpaRepository<Aviso, Long> {
     
